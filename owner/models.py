5d8447e46e7b1165fd7deb2bfbdf093b2b352ef8
yontey <muasajohn01@gmail.com>
2019-06-12 23:32:43 +0300

    created constructor app and created the models in the application

--- owner/models.py
+++ owner/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.Model)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    totalcost = models.PositiveIntegerField(default=0)
+    constructor = models.OneToOneField(Constructor,on_delete=models.CASCADE,blank=False)
+    startdate = models.DateField()
+    enddate = models.DateField() 
+    wallet = models.OneToOneField(Wallet,on_delete=models.CASCADE)
+    workers = models.PositiveIntegerField(default=0)
+    
+    
+    def __str__(self):
+        return self.name
